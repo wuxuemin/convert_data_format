ad3a5291e269a6a03978b9e5c65dbfb04c1f5ac9
wuxuemin <1739636817@qq.com>
2018-09-02 23:43:29 +0800

    Add files via upload

---
+++ read_folder/DataGridRenderer.js
@@ -0,0 +1,13 @@
+var st= new Array();
+var index=0;
+function sss(s) {
+  st[index]="";
+
+  //console.log(s);
+  st[index]=s;
+  //console.log(st[index]);
+  //console.log(index);
+  index += 1;
+  
+ 
+}
---
+++ read_folder/a.js
@@ -0,0 +1,15 @@
+// 
+//  DataGridRenderer.js
+//  Part of Mr-Data-Converter
+//  
+//  Created by Shan Carter on 2010-10-18.
+// 
+
+
+ 
+  function a() {
+for(var inx = 0 ; inx < st.length; inx++) {
+      console.log(st[inx]);
+    }
+
+}
